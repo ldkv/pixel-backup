253bdf5eb3dd84336dd4f02f7f4d1494ababd243
Vu LE <ledkvu@gmail.com>
2026-09-21 00:11:46 +0200

    feat: add access logs for admin pages

--- pixel_backup/settings.py
+++ pixel_backup/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    "pixel_backup.middleware.AccessLogMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
